6bbfba4829fec737d32d8d7f84e5a11de2a8e695
MasonChery <masonchery4@gmail.com>
2026-05-01 15:59:04 -0400

    new

--- story-app/src/components/content.js
+++ story-app/src/components/content.js
@@ -3,11 +3,13 @@ import React from "react";
 function Content() {
   return (
    <div className="content">
-      <p>Once upon a time, there was a young boy who lived in a small village. He was lazy and often played tricks on the villagers.</p>
-      
-      <p>One day, while tending to his sheep, he saw a wolf approaching. He cried out for help, but no one came. The wolf attacked the sheep, and the boy realized his mistake.</p>
-      
-      <p>From that day on, he never lied again.</p>
+      <p>Once upon a time, there was a young boy who lived in a small village. He was lazy and often played tricks on the villagers
+         he would cry out "Wolf Wolf" just for fun, even when there was no wolf around. The villagers would rush to his aid, 
+         only to find out that he was 1ying.the boy will laugh at the villagers for falling for his tricks.</p>
+         
+        <p>One day, while tending to the sheep, he saw a wolf approaching. He cried out for help, but no one came. 
+          the villagers thought it was another one of his tricks. The wolf attacked the sheep and the boy realized his mistake.
+          From that day on, he never lied again.</p>
   </div>
   );
 }
