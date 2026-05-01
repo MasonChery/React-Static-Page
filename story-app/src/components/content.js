6445a01a776a7d364be3d83bc13f27477b60e8ee
MasonChery <masonchery4@gmail.com>
2026-05-01 15:48:31 -0400

    new

---
+++ story-app/src/components/content.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Content() {
+  return (
+   <div className="content">
+      <p>Once upon a time, there was a young boy who lived in a small village. He was lazy and often played tricks on the villagers.</p>
+      
+      <p>One day, while tending to his sheep, he saw a wolf approaching. He cried out for help, but no one came. The wolf attacked the sheep, and the boy realized his mistake.</p>
+      
+      <p>From that day on, he never lied again.</p>
+  </div>
+  );
+}
+
+export default Content;
---
+++ story-app/src/components/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <h3>The Moral of the Story is:</h3>
+      <p>A lie will catch you out in the end.</p>
+    </footer>
+  );
+}
+
+export default Footer;
